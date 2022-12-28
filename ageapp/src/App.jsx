d4c538fb637d2042e05eeda0109b4ee0bb8f3bf8
Javier Vallejo <rjavier001@gmail.com>
2022-12-28 18:30:12 -0500

    function complete, html & CSS added

--- ageapp/src/App.jsx
+++ ageapp/src/App.jsx
@@ -1,10 +1,13 @@
-
 import './App.css';
+import Card from './components/Card';
+import { NavBar } from './components/NavBar';
 
 function App() {
+  const title='CALCULADORA DE EDAD'
   return (
     <div className="App">
-      
+      <NavBar title={title}/>
+      <Card />
     </div>
   );
 }
